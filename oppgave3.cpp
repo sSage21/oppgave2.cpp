0158d44999b27822850229390ea02b24f28f0a3e
Peter Åmo Nesbakk <peter.nesbakk@gmail.com>
2022-12-01 12:32:28 +0100

    En commit

---
+++ oppgave3.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cout << "Et tilfeldig tall mellom 1 og 100 er: " << 1 + (rand() % 100) << endl;
+}
